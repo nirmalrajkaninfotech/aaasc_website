81d853a193827abc427973a03eb07f0555f2805c
Tamilselvan G <kaninfotech@192.168.1.6>
2025-08-29 16:47:00 +0530

    fullapichanged

--- admin-server/src/server.js
+++ admin-server/src/server.js
@@ -65,7 +65,7 @@ app.set('trust proxy', 1);
 const allowedOrigins = [
   'http://localhost:3003',
   'http://localhost:3002',
-  'http://localhost:3000',
+  '72.60.96.178:3000',
   'http://127.0.0.1:3000',
   'http://72.60.96.178:3000',
   'http://127.0.0.1:3001',
@@ -80,7 +80,7 @@ const corsOptions = {
     const allowedOrigins = [
       'http://localhost:3003',
       'http://localhost:3002',
-      'http://localhost:3000',
+      '72.60.96.178:3000',
       'http://127.0.0.1:3000',
       'http://72.60.96.178:3000',
       'http://127.0.0.1:3001',
